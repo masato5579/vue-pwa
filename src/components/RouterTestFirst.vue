18c6701d0701939fc4cfc1210d9b92e2c6248a69
sakadume masato <namonakisizin@gmail.com>
2023-06-03 15:19:15 +0900

    [fix] vueRouterテスト

---
+++ src/components/RouterTestFirst.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>テストページ1</h2>
+    <router-link to="/second">次のページ</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RouterTestFirst",
+};
+</script>
---
+++ src/components/RouterTestSecond.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>テストページ2</h2>
+    <router-link to="/">前のページ</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RouterTestSecond",
+};
+</script>
